5a8b110d344204af33208eeff09658620970a971
ranjan3nov <ranjan3nov@gmail.com>
2024-01-09 18:54:43 +0530

    Abstract Class and methods

---
+++ 10_Abstract_Class/index.php
@@ -0,0 +1,24 @@
+<?php
+
+use Car as GlobalCar;
+
+abstract class Car
+{
+    abstract function applyBreak();
+
+    function hello()
+    {
+        echo "Hello from Car Class" . PHP_EOL;
+    }
+}
+
+class SportsCar extends Car
+{
+    function applyBreak()
+    {
+        echo "Apply Break from Sports Car" . PHP_EOL;
+    }
+}
+
+$car1 = new SportsCar();
+$car1->applyBreak();
---
+++ 10_Abstract_Class/notes.md
@@ -0,0 +1,11 @@
+# Abstract Class
+
+Key Points
+
+## Class that we do not want to create object are defined as Abstract Class
+
+## A class is called abstract when it has at least one Abstract Method inside it
+
+## Methods that are declared as Abstract must be defined in the Child Class
+
+## We cannot create instance of the abstract class
